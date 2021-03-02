587e4dc6be362894630c176831559026a0a8b13e
p-koskey <patiencekoskey@gmail.com>
2021-03-02 12:26:30 +0300

    portfolio

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import './App.css';
+import Sidebar from './components/Siderbar';
+import {useState} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Skills from './components/Skills'
+import About from './components/About'
+import Projects from './components/Projects'
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+function App() {
+  const [click, setClick] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const changeBackground = () => {
+    if (window.scrollY >= 20){
+      setNavbar(true)
+    } else{
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground)
+  return (
+    <Router>
+      <nav>
+      <div className={navbar ? 'dashboard2 scroll' : 'dashboard2'}>
+        <div className="toggle" onClick={handleClick}>
+          {click ? <CloseIcon color="primary" style={{ fontSize: 40 }}/> : <MenuIcon color="primary" style={{ fontSize: 40 }}/> }
+              
+          
+         
+        </div>
+        <div className="nav-right">
+          <div className="user">
+            <img src="avatar.png" alt="" />
+            <h3>Patience Koskey</h3>
+            <p>Web Developer</p>
+          </div>
+          
+         
+        <a href="https://drive.google.com/file/d/1HvRlX5rwhi6FKqwq-3q9K1tn7bK9zTTU/view?usp=sharing" target="_blank" rel="noreferrer"><div className="pro">
+            <h2>View My Resume</h2>
+            
+          </div></a>
+          
+        </div>
+         
+        </div>
+        <div onClick={handleClick} className="sidebar">
+          {click ? <Sidebar/> : ''}
+          
+
+        </div>
+        </nav>
+       
+       <main>
+          
+      <section className={click ? 'glass move' : 'glass'}>
+        <div className="dashboard">
+          <div className="user">
+            <img src="avatar.png" alt="" />
+            <h3>Patience Koskey</h3>
+            <p>Web Developer</p>
+          </div>
+        <Sidebar/>
+        <a href="https://drive.google.com/file/d/1HvRlX5rwhi6FKqwq-3q9K1tn7bK9zTTU/view?usp=sharing" target="_blank" rel="noreferrer"><div className="pro">
+            <h2>View My Resume</h2>
+            
+          </div></a>
+        </div>
+        
+         <div className="data">
+          <Switch>
+    <Route path='/' exact component = {About}/>
+    <Route path='/skills' component = {Skills}/>
+    <Route path='/projects' component = {Projects}/>
+    </Switch>
+        </div>
+      </section>
+      
+    </main>     
+  
+    
+    </Router>
+  );
+}
+
+export default App;
